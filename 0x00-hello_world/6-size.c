b06d7fd7485918b6287c9ec2e621910c96839526
abdulkadir ibrahim <iabdulkadir124@gmail.com>
2022-06-03 16:15:49 -0700

    6size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-* main - Prints "Programming is like building a multilingual
-* puzzle, followed by a new line."
-*
-* Return: Always 0.
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
+
