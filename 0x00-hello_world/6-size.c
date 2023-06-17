ed18a29dce989ee94291f80ce8c9148b98f24c6d
Amigoshugar <iabdulkadir124@gmail.com>
2023-06-17 06:57:57 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int main() { printf("Size of char: %zu byte(s)\nSize of short: %zu byte(s)\nSize of int: %zu byte(s)\nSize of long: %zu byte(s)\nSize of long long: %zu byte(s)\nSize of float: %zu byte(s)\nSize of double: %zu byte(s)\nSize of long double: %zu byte(s)\n", sizeof(char), sizeof(short), sizeof(int), sizeof(long), sizeof(long long), sizeof(float), sizeof(double), sizeof(long double)); return 0; }
