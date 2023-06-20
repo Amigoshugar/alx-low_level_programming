dbd7d7131c7db2fd6b2ffeba764383a0ff0c7590
Amigoshugar <iabdulkadir124@gmail.com>
2023-06-20 10:38:22 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,6 @@
-@@ -13,7 +13,7 @@ int main(void)
-	char text[10] = "Holberton";		char text[10] = "Holberton";
-	int i = 0;		int i = 0;
+#include <stdio.h>
 
-
-	for (i = 0; i < 10; i++)		for (i = 0; i < 9; i++)
-	{		{
-		_putchar(text[i]);			_putchar(text[i]);
-	}		}
-
+int main() {
+    printf("_putchar\n");
+    return 0;
+}
